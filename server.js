b3ade8333e70e26bba284bd180864222e1527ce8
lsantric <luka.santric@gmail.com>
2017-06-17 01:29:21 +0200

    Fixed second depth routing...

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 app.get('/partials/:subpath/:name', routes.subpartials);
+app.get('/partials/:sub2path/:subpath/:name', routes.sub2partials);
 
 httpsOptions = {
     key: keys.serviceKey,
