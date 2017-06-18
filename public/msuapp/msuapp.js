ef46582bac0a5518690370b87de499a213704a6d
lsantric <luka.santric@gmail.com>
2017-06-19 01:36:47 +0200

    Fixed mobile check

--- public/msuapp/msuapp.js
+++ public/msuapp/msuapp.js
@@ -2,27 +2,22 @@ var app = angular.module('msuapp', ['msuapp.controllers', 'msuapp.directives', '
 
 app.config(['$routeProvider', function($routeProvider) {
 
-	isMobile = function detectmob() {
-		if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-			return true;
-		} else {
-			return false;
-		}
-	};
+    isMobile = function detectmob() {
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-	$routeProvider.when('/', {
-		templateUrl: isMobile() ? 'partials/mobile/index' : 'partials/index',
-		controller: 'mainController',
-		resolve: {
-                isMobile: function() {
-                    return isMobile();
-                }
-            }
-	}).otherwise({
-		redirectTo: '/'
-	});
+    $routeProvider.when('/', {
+        templateUrl: isMobile() ? 'partials/mobile/index' : 'partials/index',
+        controller: 'mainController',
+    }).otherwise({
+        redirectTo: '/'
+    });
 }]);
 
 app.config(['$locationProvider', function($locationProvider) {
-	$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 }]);
